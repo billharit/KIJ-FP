1f271c32f0d74940ab966714a3896922ff5adbe2
Muhammad Zhafran <frans.zhafran@gmail.com>
2022-12-11 17:33:23 +0700

    chore: aes and ecb

--- aes.go
+++ aes.go
@@ -40,6 +40,40 @@ func (s *SBox) Generate(data []string) {
 	}
 }
 
+type SBoxReverse struct {
+	data map[byte]byte
+}
+
+func (s *SBoxReverse) Init() {
+	s.data = make(map[byte]byte)
+	s.Generate([]string{"52", "09", "6a", "d5", "30", "36", "a5", "38", "bf", "40", "a3", "9e", "81", "f3", "d7", "fb",
+		"7c", "e3", "39", "82", "9b", "2f", "ff", "87", "34", "8e", "43", "44", "c4", "de", "e9", "cb",
+		"54", "7b", "94", "32", "a6", "c2", "23", "3d", "ee", "4c", "95", "0b", "42", "fa", "c3", "4e",
+		"08", "2e", "a1", "66", "28", "d9", "24", "b2", "76", "5b", "a2", "49", "6d", "8b", "d1", "25",
+		"72", "f8", "f6", "64", "86", "68", "98", "16", "d4", "a4", "5c", "cc", "5d", "65", "b6", "92",
+		"6c", "70", "48", "50", "fd", "ed", "b9", "da", "5e", "15", "46", "57", "a7", "8d", "9d", "84",
+		"90", "d8", "ab", "00", "8c", "bc", "d3", "0a", "f7", "e4", "58", "05", "b8", "b3", "45", "06",
+		"d0", "2c", "1e", "8f", "ca", "3f", "0f", "02", "c1", "af", "bd", "03", "01", "13", "8a", "6b",
+		"3a", "91", "11", "41", "4f", "67", "dc", "ea", "97", "f2", "cf", "ce", "f0", "b4", "e6", "73",
+		"96", "ac", "74", "22", "e7", "ad", "35", "85", "e2", "f9", "37", "e8", "1c", "75", "df", "6e",
+		"47", "f1", "1a", "71", "1d", "29", "c5", "89", "6f", "b7", "62", "0e", "aa", "18", "be", "1b",
+		"fc", "56", "3e", "4b", "c6", "d2", "79", "20", "9a", "db", "c0", "fe", "78", "cd", "5a", "f4",
+		"1f", "dd", "a8", "33", "88", "07", "c7", "31", "b1", "12", "10", "59", "27", "80", "ec", "5f",
+		"60", "51", "7f", "a9", "19", "b5", "4a", "0d", "2d", "e5", "7a", "9f", "93", "c9", "9c", "ef",
+		"a0", "e0", "3b", "4d", "ae", "2a", "f5", "b0", "c8", "eb", "bb", "3c", "83", "53", "99", "61",
+		"17", "2b", "04", "7e", "ba", "77", "d6", "26", "e1", "69", "14", "63", "55", "21", "0c", "7d"})
+}
+
+func (s *SBoxReverse) Generate(data []string) {
+	for i, v := range data {
+		hexVal, err := hex.DecodeString(v)
+		if err != nil {
+			panic(err)
+		}
+		s.data[byte(i)] = hexVal[0]
+	}
+}
+
 type Matrix struct {
 	data [][]byte
 }
@@ -82,6 +116,15 @@ func (m *Matrix) Subtitute() {
 	}
 }
 
+func (m *Matrix) InverseSubtitute() {
+	for i, row := range m.data {
+		for k, b := range row {
+			// log.Printf("Changing %02x -> %02x", b, sbox.data[b])
+			m.data[i][k] = sboxReverse.data[b]
+		}
+	}
+}
+
 func (m *Matrix) RoundShift() {
 	for i, row := range m.data {
 		newRow := m.shiftRow(i, row)
@@ -95,8 +138,23 @@ func (m *Matrix) shiftRow(shiftCount int, row []byte) []byte {
 	newRow := make([]byte, len(row))
 	copy(newRow, row)
 	newRow = append(newRow, newRow...)
-	// log.Println(newRow)
 	return newRow[shiftCount : 4+shiftCount]
+}
+
+func (m *Matrix) InverseRoundShift() {
+	for i, row := range m.data {
+		newRow := m.inverseShiftRow(i, row)
+		for k, v := range newRow {
+			m.data[i][k] = v
+		}
+	}
+}
+
+func (m *Matrix) inverseShiftRow(shiftCount int, row []byte) []byte {
+	newRow := make([]byte, len(row))
+	copy(newRow, row)
+	newRow = append(newRow, newRow...)
+	return newRow[len(row)-shiftCount : 4+(len(row)-shiftCount)]
 }
 
 func (m *Matrix) MixColumn() {
@@ -108,26 +166,44 @@ func (m *Matrix) MixColumn() {
 	}
 
 	multiply := func(a byte, b byte) byte {
-		if b == 1 {
-			return a
+		// a7 = bit 7 from a
+		// b0 = bit 0 from b
+		var p byte
+		r := byte(1<<4 | 1<<3 | 1<<1 | 1<<0)
+		for i := 0; i < 8; i++ {
+			ab0 := a &^ (b&1 - 1)  // ab0 = Mul(a, b0)
+			ra7 := r &^ (a>>7 - 1) // ra7 = Mul(R, a7)
+
+			// p = Add(p, ab0)
+			// a = Sub(Mul(a, x), ra7)
+			// b = Div(b, x)
+			p, a, b = p^ab0, a<<1^ra7, b>>1
 		}
-		tmp := (a << 1) & 0xff
-		if b == 2 {
-			if a < 128 {
-				return tmp
-			} else {
-				return tmp ^ 0x1b
-			}
-		}
-		if b == 3 {
-			if a < 128 {
-				return tmp ^ a
-			} else {
-				return (tmp ^ 0x1b) ^ a
-			}
-		}
-		panic("aneh")
+		return p
 	}
+
+	// multiply := func(a byte, b byte) byte {
+	// 	if b == 1 {
+	// 		return a
+	// 	}
+	// 	tmp := (a << 1) & 0xff
+	// 	if b == 2 {
+	// 		if a < 128 {
+	// 			return tmp
+	// 		} else {
+	// 			return tmp ^ 0x1b
+	// 		}
+	// 	}
+	// 	if b == 3 {
+	// 		if a < 128 {
+	// 			return tmp ^ a
+	// 		} else {
+	// 			return (tmp ^ 0x1b) ^ a
+	// 		}
+	// 	}
+	// 	panic("aneh")
+	// }
+
 	newRow := [][]byte{}
 	n := 0
 	for n < 4 {
@@ -162,6 +238,58 @@ func (m *Matrix) MixColumn() {
 	m.data = newRow
 }
 
+func (m *Matrix) InverseMixColumn() {
+	fixedMatrix := [][]byte{
+		{14, 11, 13, 9},
+		{9, 14, 11, 13},
+		{13, 9, 14, 11},
+		{11, 13, 9, 14},
+	}
+
+	multiply := func(a byte, b byte) byte {
+		// a7 = bit 7 from a
+		// b0 = bit 0 from b
+		var p byte
+		r := byte(1<<4 | 1<<3 | 1<<1 | 1<<0)
+		for i := 0; i < 8; i++ {
+			ab0 := a &^ (b&1 - 1)  // ab0 = Mul(a, b0)
+			ra7 := r &^ (a>>7 - 1) // ra7 = Mul(R, a7)
+
+			// p = Add(p, ab0)
+			// a = Sub(Mul(a, x), ra7)
+			// b = Div(b, x)
+			p, a, b = p^ab0, a<<1^ra7, b>>1
+		}
+		return p
+	}
+
+	// region - init new state matrix
+	newRow := [][]byte{}
+	n := 0
+	for n < 4 {
+		newRow = append(newRow, []byte{})
+		n++
+	}
+	// region - init new state matrix
+
+	for i, row := range m.data {
+		nowRow := []byte{}
+		for k := range row {
+			var newValue byte
+			for n := 0; n < 4; n++ {
+				if n == 0 {
+					newValue = multiply(fixedMatrix[i][n], m.data[n][k])
+				} else {
+					newValue = newValue ^ multiply(fixedMatrix[i][n], m.data[n][k])
+				}
+			}
+			nowRow = append(nowRow, newValue)
+		}
+		newRow[i] = nowRow
+	}
+	m.data = newRow
+}
+
 func (m *Matrix) Print() {
 	fmt.Printf("===========\n")
 	for _, row := range m.data {
@@ -173,6 +301,50 @@ func (m *Matrix) Print() {
 		}
 		fmt.Printf("\n")
 	}
+	fmt.Printf("===========\n")
+}
+
+func (m *Matrix) PrintString() {
+	fmt.Printf(" === ")
+	for i := 0; i < len(m.data); i++ {
+		for j := 0; j < len(m.data); j++ {
+			fmt.Printf("%02x", m.data[j][i])
+		}
+		fmt.Printf(" ")
+	}
+	fmt.Printf("\n")
+}
+
+func (m *Matrix) String() string {
+	result := ""
+	for i := 0; i < len(m.data); i++ {
+		for j := 0; j < len(m.data); j++ {
+			result += fmt.Sprintf("%02x", m.data[j][i])
+		}
+		result += fmt.Sprintf(" ")
+	}
+	return result
+}
+
+// func Pretty(input []byte) string {
+// 	result := ""
+// 	for i := 0; i < len(input); i++ {
+// 		for j := 0; j < len(input); j++ {
+// 			result += fmt.Sprintf("%02x", m.data[j][i])
+// 		}
+// 		result += fmt.Sprintf(" ")
+// 	}
+// 	return result
+// }
+
+func (m *Matrix) AsByte() []byte {
+	result := []byte{}
+	for i := 0; i < len(m.data); i++ {
+		for j := 0; j < len(m.data); j++ {
+			result = append(result, m.data[j][i])
+		}
+	}
+	return result
 }
 
 func gFunc(constant byte, key []byte) []byte {
@@ -199,6 +371,7 @@ func matrixXor(a []byte, b []byte) []byte {
 }
 
 var sbox *SBox
+var sboxReverse *SBoxReverse
 
 func preComputeRoundConstant(n int) {
 	i := 0
@@ -254,22 +427,20 @@ func shiftMatrix(matrix []byte) {
 	}
 }
 
-// func mixColumn(matrix []byte) {
-// 	for i, v := range matrix {
-
-// 	}
-// }
-
 func shiftRow(shiftCount uint8, row []byte) []byte {
 	row = append(row, row...)
 	return row[shiftCount : 3+shiftCount]
 }
 
-func main() {
+func encryptECBBlock(plainText []byte, key []byte) []byte {
 	// region - Rjindael Sbox
 	sboxLocal := &SBox{}
 	sboxLocal.Init()
 	sbox = sboxLocal
+
+	sboxReverseLocal := &SBoxReverse{}
+	sboxReverseLocal.Init()
+	sboxReverse = sboxReverseLocal
 	// region - Rjindael Sbox
 
 	totalRound := 10
@@ -277,15 +448,6 @@ func main() {
 	// region - Compute Round Constant for Key Expansion
 	preComputeRoundConstant(totalRound)
 	// region - Compute Round Constant for Key Expansion
-
-	plainText := []byte("Two One Nine Two")
-	key := []byte("Thats my Kung Fu")
-	log.Println("Key size:", len(key)*8)
-
-	encodedString := hex.EncodeToString(plainText)
-	log.Println(encodedString)
-	encodedString = hex.EncodeToString(key)
-	log.Println(encodedString)
 
 	// region - Key Expansion
 	words := [][]byte{}
@@ -312,28 +474,10 @@ func main() {
 	}
 	// region - Key Expansion
 
-	log.Println(len(words))
-	buffer := []byte{}
-	for i, b := range words {
-		if i%4 == 0 {
-			fmt.Printf("Round: %d ", (i/4)-1)
-			for _, bf := range buffer {
-				fmt.Printf("%02x ", bf)
-			}
-			fmt.Println("")
-			buffer = []byte{}
-		}
-		for _, bc := range b {
-			buffer = append(buffer, bc)
-		}
-	}
-	log.Println("Finished")
-
 	// region - Prepare plaintext as matrix
 	matrix := &Matrix{}
 	matrix.Init()
 	matrix.Fill(plainText)
-	matrix.Print()
 	// region - Prepare plaintext as matrix
 
 	// region - Encryption
@@ -341,17 +485,17 @@ func main() {
 	for round <= totalRound {
 		if round > 0 {
 			matrix.Subtitute()
-			log.Println("After subtitute", round)
-			matrix.Print()
+			// log.Println("After subtitute", round)
+			// matrix.Print()
 
 			matrix.RoundShift()
-			log.Println("After shift", round)
-			matrix.Print()
+			// log.Println("After shift", round)
+			// matrix.Print()
 
 			if round < totalRound {
 				matrix.MixColumn()
-				log.Println("After mixCol", round)
-				matrix.Print()
+				// log.Println("After mixCol", round)
+				// matrix.Print()
 			}
 		}
 
@@ -361,11 +505,146 @@ func main() {
 		keyMatrix.Fill(key)
 
 		matrix.XOR(keyMatrix)
-		log.Println("XOR", round)
-
-		matrix.Print()
+		// log.Println("After round key", round)
+		// matrix.Print()
 
 		round++
 	}
 	// region - Encryption
+
+	return matrix.AsByte()
+}
+
+func decryptECBBlock(cipherText []byte, key []byte) []byte {
+	// region - Algo Properties
+	round := 0
+	totalRound := 10
+	decryptRound := 10
+	// region - Algo Properties
+
+	// region - Key Expansion
+	words := [][]byte{}
+
+	words = append(words, key[0:4])
+	words = append(words, key[4:8])
+	words = append(words, key[8:12])
+	words = append(words, key[12:16])
+
+	roundConstant := 0
+	for {
+		// docs - Expand key for 4*totalRound times
+		if len(words) == (totalRound+1)*4 {
+			break
+		}
+		last := len(words) - 1
+		if len(words)%4 == 0 {
+			gMatrix := gFunc(byte(roundConstant), words[last])
+			words = append(words, matrixXor(words[last-3], gMatrix)) // word 4
+			roundConstant++
+		} else {
+			words = append(words, matrixXor(words[last], words[last-3]))
+		}
+	}
+	// region - Key Expansion
+
+	// region - Decryption
+	matrix := &Matrix{}
+	matrix.Init()
+	matrix.Fill(cipherText)
+	for round <= totalRound {
+		if round > 0 {
+			matrix.InverseRoundShift()
+			// log.Println("Decrypt after shift", round)
+			// matrix.Print()
+			// matrix.PrintString()
+
+			matrix.InverseSubtitute()
+			// log.Println("Decrypt after subtitute", round)
+			// matrix.Print()
+			// matrix.PrintString()
+
+			if decryptRound >= 0 {
+				key = joinMatrix(words[(decryptRound * 4) : (decryptRound*4)+4]...)
+				keyMatrix := &Matrix{}
+				keyMatrix.Init()
+				keyMatrix.Fill(key)
+				// log.Println("Decrypt AddRoundKey", decryptRound)
+				// keyMatrix.PrintString()
+
+				matrix.XOR(keyMatrix)
+				// matrix.Print()
+				// matrix.PrintString()
+
+				if decryptRound > 0 {
+					matrix.InverseMixColumn()
+					// log.Println("Decrypt after mixCol", round)
+					// matrix.Print()
+					// matrix.PrintString()
+				}
+			}
+		}
+
+		if round == 0 {
+			key = joinMatrix(words[(decryptRound * 4) : (decryptRound*4)+4]...)
+			keyMatrix := &Matrix{}
+			keyMatrix.Init()
+			keyMatrix.Fill(key)
+			// log.Println("Decrypt AddRoundKey", decryptRound)
+
+			matrix.XOR(keyMatrix)
+			// matrix.Print()
+			// matrix.PrintString()
+		}
+		round++
+		decryptRound--
+	}
+
+	return matrix.AsByte()
+}
+
+func encryptECB(input []byte, key []byte) []byte {
+	result := []byte{}
+	iteration := 0
+	for {
+		block := encryptECBBlock(input[iteration*16:(iteration+1)*16], key)
+		result = append(result, block...)
+		iteration++
+		if len(input) == (iteration)*16 {
+			break
+		}
+	}
+	return result
+}
+
+func decryptECB(input []byte, key []byte) []byte {
+	result := []byte{}
+	iteration := 0
+	for {
+		block := decryptECBBlock(input[iteration*16:(iteration+1)*16], key)
+		result = append(result, block...)
+		iteration++
+		if len(input) == (iteration)*16 {
+			break
+		}
+	}
+	return result
+}
+
+func main() {
+	plainText := "Two One Nine Two"
+	key := "Thats my Kung Fu"
+	fmt.Println("Plain text:", plainText)
+	fmt.Println("Key:", key)
+
+	encrypted := encryptECBBlock([]byte(plainText), []byte(key))
+	decrypted := decryptECBBlock(encrypted, []byte(key))
+
+	fmt.Println("")
+	fmt.Println("Plaintext after decrypt (hex):", hex.EncodeToString(decrypted))
+	fmt.Println("Plaintext after decrypt:", string(decrypted))
+
+	encryptedCBC := encryptECB([]byte(plainText+plainText), []byte(key))
+	decryptedCBC := decryptECB(encryptedCBC, []byte(key))
+	fmt.Println("Plaintext after decrypt ECB (hex):", hex.EncodeToString(decryptedCBC))
+	fmt.Println("Plaintext after decrypt ECB:", string(decryptedCBC))
 }
